7b54206cbe85cf89dede816184ba5720ba62ab77
Mersudin Delic <mdadelic@gmail.com>
2022-06-05 23:54:38 +0200

    added comments to all api methods

--- QuizService/Controllers/QuizController.cs
+++ QuizService/Controllers/QuizController.cs
@@ -13,6 +13,11 @@ namespace QuizService.Controllers;
 [Route("api/quizzes")]
 public class QuizController : Controller
 {
+    /*
+     * TODO I would use a repository for dealing with the database and a service for the business logic everywhere, 
+     * because that way the code is much more readable and maintainable when the project becomes huge
+    */
+
     private readonly IDbConnection _connection;
     private readonly IQuizzesService _service;
 
@@ -52,8 +57,13 @@ public class QuizController : Controller
 
     // POST api/quizzes
     [HttpPost]
-    public IActionResult Post([FromBody]QuizCreateModel value)
+    public IActionResult Post([FromBody] QuizCreateModel value)
     {
+        /*
+         * TODO 1. I would add data validations part and implement exceptions handling
+         * TODO 2. I would return a specific DTO which contains neded data and a proper message
+         * for the client if process is done successfully or not
+         */
         var sql = $"INSERT INTO Quiz (Title) VALUES('{value.Title}'); SELECT LAST_INSERT_ROWID();";
         var id = _connection.ExecuteScalar(sql);
         return Created($"/api/quizzes/{id}", null);
@@ -61,10 +71,15 @@ public class QuizController : Controller
 
     // PUT api/quizzes/5
     [HttpPut("{id}")]
-    public IActionResult Put(int id, [FromBody]QuizUpdateModel value)
+    public IActionResult Put(int id, [FromBody] QuizUpdateModel value)
     {
+        /*
+         * TODO 1. I would add data validations part and implement exceptions handling
+         * TODO 2. I would return a specific DTO which contains data if needed and a proper message
+         * for the client if process is done successfully or not
+         */
         const string sql = "UPDATE Quiz SET Title = @Title WHERE Id = @Id";
-        int rowsUpdated = _connection.Execute(sql, new {Id = id, Title = value.Title});
+        int rowsUpdated = _connection.Execute(sql, new { Id = id, Title = value.Title });
         if (rowsUpdated == 0)
             return NotFound();
         return NoContent();
@@ -74,8 +89,12 @@ public class QuizController : Controller
     [HttpDelete("{id}")]
     public IActionResult Delete(int id)
     {
+        /*
+         * TODO 1. I would return a specific DTO which contains a proper message
+         * for the client if process is done successfully or not
+         */
         const string sql = "DELETE FROM Quiz WHERE Id = @Id";
-        int rowsDeleted = _connection.Execute(sql, new {Id = id});
+        int rowsDeleted = _connection.Execute(sql, new { Id = id });
         if (rowsDeleted == 0)
             return NotFound();
         return NoContent();
@@ -84,19 +103,29 @@ public class QuizController : Controller
     // POST api/quizzes/5/questions
     [HttpPost]
     [Route("{id}/questions")]
-    public IActionResult PostQuestion(int id, [FromBody]QuestionCreateModel value)
+    public IActionResult PostQuestion(int id, [FromBody] QuestionCreateModel value)
     {
+        /*
+         * TODO 1. I would add data validations part and implement exceptions handling
+         * TODO 2. I would return a specific DTO which contains data if needed and a proper message
+         * for the client if process is done successfully or not
+         */
         const string sql = "INSERT INTO Question (Text, QuizId) VALUES(@Text, @QuizId); SELECT LAST_INSERT_ROWID();";
-        var questionId = _connection.ExecuteScalar(sql, new {Text = value.Text, QuizId = id});
+        var questionId = _connection.ExecuteScalar(sql, new { Text = value.Text, QuizId = id });
         return Created($"/api/quizzes/{id}/questions/{questionId}", null);
     }
 
     // PUT api/quizzes/5/questions/6
     [HttpPut("{id}/questions/{qid}")]
-    public IActionResult PutQuestion(int id, int qid, [FromBody]QuestionUpdateModel value)
+    public IActionResult PutQuestion(int id, int qid, [FromBody] QuestionUpdateModel value)
     {
+        /*
+         * TODO 1. I would add data validations part and implement exceptions handling
+         * TODO 2. I would return a specific DTO which contains data if needed and proper message
+         * for the client if process is done successfully or not
+         */
         const string sql = "UPDATE Question SET Text = @Text, CorrectAnswerId = @CorrectAnswerId WHERE Id = @QuestionId";
-        int rowsUpdated = _connection.Execute(sql, new {QuestionId = qid, Text = value.Text, CorrectAnswerId = value.CorrectAnswerId});
+        int rowsUpdated = _connection.Execute(sql, new { QuestionId = qid, Text = value.Text, CorrectAnswerId = value.CorrectAnswerId });
         if (rowsUpdated == 0)
             return NotFound();
         return NoContent();
@@ -107,27 +136,41 @@ public class QuizController : Controller
     [Route("{id}/questions/{qid}")]
     public IActionResult DeleteQuestion(int id, int qid)
     {
+        /*
+         * TODO 1. I would return a specific DTO which contains a proper message
+         * for the client if process is done successfully or not
+         */
         const string sql = "DELETE FROM Question WHERE Id = @QuestionId";
-        _connection.ExecuteScalar(sql, new {QuestionId = qid});
+        _connection.ExecuteScalar(sql, new { QuestionId = qid });
         return NoContent();
     }
 
     // POST api/quizzes/5/questions/6/answers
     [HttpPost]
     [Route("{id}/questions/{qid}/answers")]
-    public IActionResult PostAnswer(int id, int qid, [FromBody]AnswerCreateModel value)
+    public IActionResult PostAnswer(int id, int qid, [FromBody] AnswerCreateModel value)
     {
+        /*
+         * TODO 1. I would add data validations part and implement exceptions handling
+         * TODO 2. I would return a specific DTO which contains data if needed and a proper message
+         * for the client if process is done successfully or not
+         */
         const string sql = "INSERT INTO Answer (Text, QuestionId) VALUES(@Text, @QuestionId); SELECT LAST_INSERT_ROWID();";
-        var answerId = _connection.ExecuteScalar(sql, new {Text = value.Text, QuestionId = qid});
+        var answerId = _connection.ExecuteScalar(sql, new { Text = value.Text, QuestionId = qid });
         return Created($"/api/quizzes/{id}/questions/{qid}/answers/{answerId}", null);
     }
 
     // PUT api/quizzes/5/questions/6/answers/7
     [HttpPut("{id}/questions/{qid}/answers/{aid}")]
-    public IActionResult PutAnswer(int id, int qid, int aid, [FromBody]AnswerUpdateModel value)
+    public IActionResult PutAnswer(int id, int qid, int aid, [FromBody] AnswerUpdateModel value)
     {
+        /*
+         * TODO 1. I would add data validations part and implement exceptions handling
+         * TODO 2. I would return a specific DTO which contains data if needed and proper message
+         * for the client if process is done successfully or not
+         */
         const string sql = "UPDATE Answer SET Text = @Text WHERE Id = @AnswerId";
-        int rowsUpdated = _connection.Execute(sql, new {AnswerId = qid, Text = value.Text});
+        int rowsUpdated = _connection.Execute(sql, new { AnswerId = qid, Text = value.Text });
         if (rowsUpdated == 0)
             return NotFound();
         return NoContent();
@@ -138,8 +181,12 @@ public class QuizController : Controller
     [Route("{id}/questions/{qid}/answers/{aid}")]
     public IActionResult DeleteAnswer(int id, int qid, int aid)
     {
+        /*
+         * TODO 1. I would return a specific DTO which contains a proper message
+         * for the client if process is done successfully or not
+         */
         const string sql = "DELETE FROM Answer WHERE Id = @AnswerId";
-        _connection.ExecuteScalar(sql, new {AnswerId = aid});
+        _connection.ExecuteScalar(sql, new { AnswerId = aid });
         return NoContent();
     }
 }
